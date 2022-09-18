5bd9ca4308bd3dac30ec3034258b183eedd1a42f
sindy <sindy@SINDY_ARCE>
2022-09-17 23:38:28 -0600

    Agregar archivos de proyecto.

---
+++ Multi_Propiedades/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Propiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            //Objeto
+            clmulti pMultip = new clmulti();
+            pMultip.Nume1 = int.Parse(txtNum1.Text);
+            pMultip.Nume2 = int.Parse(txtNum2.Text);
+            textBox3.Text = pMultip.multip().ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ Multi_Propiedades/clmulti.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Propiedades
+{
+    class clmulti
+    {
+        //Atributos
+        private int num1;
+        private int num2;
+
+        //Propiedades
+        public int Nume1
+        {
+            get => num1; set => num1 = value;
+        }
+        public int Nume2
+        {
+            get => num2; set => num2 = value;
+        }
+
+        //Método
+        public int multip()
+        {
+            return (num1 * num2);
+        }
+    }
+}
